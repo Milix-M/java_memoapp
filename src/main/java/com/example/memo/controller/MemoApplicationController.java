f7343f119fdbba016fac42c210147b170b630974
Milix-M <tujikou12dh@gmail.com>
2024-04-25 16:59:33 +0900

    add MemoService DI

--- src/main/java/com/example/memo/controller/MemoApplicationController.java
+++ src/main/java/com/example/memo/controller/MemoApplicationController.java
@@ -1,11 +1,17 @@
 package com.example.memo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.memo.service.MemoService;
 
 
 @Controller
 public class MemoApplicationController {
+
+    @Autowired
+    private MemoService memoService;
 
     @GetMapping("/")
     public String index() {
