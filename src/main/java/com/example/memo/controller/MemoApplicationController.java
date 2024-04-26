596b09c3ade449f95ec346f7e5cf597b5728fc4c
Milix-M <tujikou12dh@gmail.com>
2024-04-26 11:48:47 +0900

    メモを追加できるように変更

--- src/main/java/com/example/memo/controller/MemoApplicationController.java
+++ src/main/java/com/example/memo/controller/MemoApplicationController.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.memo.responce.Memo;
+import com.example.memo.responce.MemoForm;
 import com.example.memo.responce.MemoList;
 import com.example.memo.service.MemoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class MemoApplicationController {
@@ -25,6 +31,16 @@ public class MemoApplicationController {
     @GetMapping("/add")
     public String addMemo() {
         return "addmemo";
+    }
+
+    @PostMapping("/add")
+    public String addMemo(@ModelAttribute("MemoForm") MemoForm form, Model model) {
+        Memo memo = new Memo();
+        memo.setTitle(form.getTitle());
+        memo.setText(form.getText());
+        memoService.createMemo(memo);
+
+        return "redirect:/";
     }
 
     @GetMapping("/memo")
