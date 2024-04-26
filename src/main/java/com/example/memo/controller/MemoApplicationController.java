906b49ab9eabb312892981778fc04ae2020fddd4
Milix-M <tujikou12dh@gmail.com>
2024-04-26 10:34:47 +0900

    index.htmlにmemoのtitleを渡せるように

--- src/main/java/com/example/memo/controller/MemoApplicationController.java
+++ src/main/java/com/example/memo/controller/MemoApplicationController.java
@@ -2,10 +2,10 @@ package com.example.memo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.example.memo.responce.MemoList;
 import com.example.memo.service.MemoService;
-
 
 @Controller
 public class MemoApplicationController {
@@ -14,7 +14,11 @@ public class MemoApplicationController {
     private MemoService memoService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        MemoList memoList = new MemoList();
+        memoList.setMemoList(memoService.getMemoList());
+        model.addAttribute("memolist", memoList);
+
         return "index";
     }
 
