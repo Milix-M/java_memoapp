4050cdd3c1a5c8ee29bae7765995d88788492a77
Milix-M <tujikou12dh@gmail.com>
2024-04-26 11:28:19 +0900

    add MemoForm.java

---
+++ src/main/java/com/example/memo/responce/MemoForm.java
@@ -0,0 +1,13 @@
+package com.example.memo.responce;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemoForm {
+
+    private String title;
+
+    private String text;
+}
